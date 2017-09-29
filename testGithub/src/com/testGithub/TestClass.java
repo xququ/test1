3d163ff08465775a58cc98b899b6ecbf50ec82c2
xququ <x_ququ@yahoo.com>
2017-09-28 22:05:04 -0400

    Test Project

---
+++ testGithub/src/com/testGithub/TestClass.java
@@ -0,0 +1,10 @@
+package com.testGithub;
+
+public class TestClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Helo There... Testing GitHub");
+	}
+
+}
