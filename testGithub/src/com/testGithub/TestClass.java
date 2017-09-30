026f7f4787f74c175c9b80fd217e9a9f2d48d133
xququ <x_ququ@yahoo.com>
2017-09-29 23:35:52 -0400

    Test2

--- testGithub/src/com/testGithub/TestClass.java
+++ testGithub/src/com/testGithub/TestClass.java
@@ -5,6 +5,7 @@ public class TestClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Helo There... Testing GitHub1");
+		System.out.println("From Test 2");
 	}
 
 }
